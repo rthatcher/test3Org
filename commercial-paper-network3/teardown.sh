8cc1d7b46382c3cff4228998791f0103c3dc1991
rthatcher <robert.thatcher@uk.ibm.com>
2019-03-11 14:45:53 +0000

    created initial files
    Signed-off-by: rthatcher <robert.thatcher@uk.ibm.com>

---
+++ commercial-paper-network3/teardown.sh
@@ -0,0 +1,31 @@
+# Ask user for confirmation to proceed
+function askProceed() {
+  echo "========================== Warning =============================="
+  echo "This script will stop and remove all locally running containers, "
+  echo "and remove all local chaincode images from all Fabric networks"
+  echo "================================================================="
+  read -p "Continue? [Y/n] " ans
+  case "$ans" in
+  y | Y | "")
+    echo "proceeding ..."
+    ;;
+  n | N)
+    echo "exiting..."
+    exit 1
+    ;;
+  *)
+    echo "invalid response"
+    askProceed
+    ;;
+  esac
+}
+
+askProceed
+
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi $(docker images dev* -q)
+docker volume prune -f
+docker network prune -f
+
+
